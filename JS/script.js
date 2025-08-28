24703ed88449251b6defdf3214fb477fc68aaec4
clorensia <106355113+clorensia@users.noreply.github.com>
2025-08-28 09:22:54 +0700

    belajar function

--- JS/script.js
+++ JS/script.js
@@ -57,3 +57,38 @@ for (let i = 10; i < 0; i--){
 //while (gabisa dihitung)
 
 // do while (gabisa dihitung)
+
+// funbtion classic func. variable function. arroe function
+function sayHello(name) {
+    console.log("Gello, HI!" + name)
+}
+
+sayHello("candra"); // argumen: proses memasukkan inputan, yang akan dimasukan ke parameter
+
+//function membuat luas lingkaran
+function luasLingkaran(jariJari) {
+    const pi = Math.PI;
+    //rumus luas lingkarab: pi * jari.jari * jarijari
+    const luas = pi * jariJari * jariJari;
+    return luas; //ngembaliin nilai. bisa keluar alias global scopes. nggak wajib ada, sesuai kebutuhan.
+}
+console.log(luasLingkaran(9)) //buat ngeprint
+
+// menjumlahkan bil genap dari a -> b
+// a = 4, b = 10 | 4 6 8 10 => 28
+// Deklarasi variabel a dan b
+const c = 4;
+const d = 10;
+let total = 0; // Variabel untuk menyimpan hasil penjumlahan!!
+
+// Loop dari nilai 'a' hingga 'b'
+for (let i = c; i <= d; i++) {
+  // Cek apakah angka saat ini adalah bilangan genap
+  if (i % 2 === 0) {
+    // Jika ya, tambahkan ke variabel 'total'
+    total = total + i;
+  }
+}
+
+// Menampilkan hasil
+console.log(total); // Output: 28
